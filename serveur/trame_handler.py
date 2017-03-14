2bc41672b4070889338447d00627d625258abc11
malocharo <poles.malo@gmail.com>
2017-03-14 14:42:47 +0100

    integration du demon
    Signed-off-by: malocharo <poles.malo@gmail.com>

--- serveur/trame_handler.py
+++ serveur/trame_handler.py
@@ -14,7 +14,7 @@ class DataHandler:
 
     def _identify(self):
         if self.data[0] == 0x10:
-            aff = afficheur.Afficheur("192.168.15.10", 502)
+            aff = afficheur.Afficheur("192.168.15.10", 502) # a
             aff.msg(self.data[1:])
         elif self.data[0] == 0xFF:
             watchdog.Watchdog(self.data)
