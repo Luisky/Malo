1c11c884a406fa0b0b9a8695bef5577bad505ffa
malocharo <poles.malo@gmail.com>
2017-03-14 13:50:30 +0100

    mise en place watchdog
    Signed-off-by: malocharo <poles.malo@gmail.com>

--- serveur/trame_handler.py
+++ serveur/trame_handler.py
@@ -12,10 +12,9 @@ class DataHandler:
 
     def _identify(self):
         if self.data[0] == 0x10:
-            print("ok")
-            print(self.data[1:])
             aff = afficheur.Afficheur("192.168.15.10", 502)
             aff.msg(self.data[1:])
-
+        elif self.data[0] == 0xFF:
+            
 
 
